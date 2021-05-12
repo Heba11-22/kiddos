07a073c59c2b9a06728b4e48e27ee50e76ae022e
Heba <hkasaby83@gmail.com>
2021-05-12 11:54:36 -0500

    m

--- react-app/src/components/SavedItems/index.js
+++ react-app/src/components/SavedItems/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from "react-router-dom";
 import LoginForm from "../../components/LoginSignUpForm/LoginForm";
 import SignUpForm from "../../components/LoginSignUpForm/SignUpForm";
 import LogSign from "../LogSign";
@@ -19,7 +20,7 @@ function SavedItems() {
   const user = useSelector(state => state.session.user) || {}
   const userId = user.id
   const [loaded, setLoaded] = useState(false);
-  const allItemsValues = Object.values(allItems)
+  const allItemsValues = Object.values(allItems) || {}
   console.log("////////////", allItemsValues)
 
   // useEffect cant't have async func. Only if we invoke it immeditaly.
@@ -36,15 +37,20 @@ function SavedItems() {
   return (
     <div className="saved-items-div">
       <div>HHHHHHIIIIIII</div>
-      {/* <ul className="saved-items-ul">
+      <ul className="saved-items-ul">
         {allItemsValues.map((item,i) => (
           <li key={i}>
-            <h5>{item}</h5>
+          <NavLink to={`/items/${item.id}`}>
+            <h5>{item.itemName}</h5>
+          </NavLink>
+          <NavLink to={`/items/${item.id}`}>
+            <img className="saved-items-img" src={item.photos.photo_url}></img>
+          </NavLink>
             {console.log("?????????", item)}
           </li>
         ))}
 
-      </ul> */}
+      </ul>
    </div>
   )
 }
