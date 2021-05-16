e8bae627b15f075c4a30a97e0956ff20e1dbb742
Heba <hkasaby83@gmail.com>
2021-05-16 02:17:25 -0500

    working on item page

--- react-app/src/components/Items/index.js
+++ react-app/src/components/Items/index.js
@@ -9,13 +9,15 @@ import { allMainCategories } from '../../store/mainCategories'
 import LoginSignUpModal from '../LoginSignUpForm'
 import { Modal } from '../../context/Modal';
 import { saveAnItemThunk } from "../../store/savedItems";
+import "./Items.css"
 
 
 function SingleItem() {
     const dispatch = useDispatch();
     const history = useHistory()
     const user = useSelector(state => state.session.user)
-    // console.log("USEEEEER",user)
+    const items = useSelector(state => state.mainCategories) 
+    console.log("USEEEEER",items)
     // const userId = user.id || {}
     const [item, setItem] = useState({})
     const [targtedItem, setTargtedItem] = useState()
@@ -43,7 +45,7 @@ const itemValues = Object.values(item)[0] || {}
 let photo_url = (Object.values(itemValues))[6] || {}
 // console.log(photo_url.photo_url)
 let sizesArray = itemValues.sizes || {};
-// console.log(itemValues)
+console.log("IIIIIIIIIIIII",item)
 // console.log(sizesArray)
 
 // let savedItemsArray = []
@@ -71,28 +73,48 @@ const handleSavedItems = (e) => {
 
     return (
         <div className="single-item">
-        {showModal && (
-            <div>
-                <Modal onClose={() => setShowModal(false)}>
-                    <LoginSignUpModal/>
-                </Modal> 
-            </div>
-        )}
             {/* <LoginSignUpModal/> */}
-            <div className="item-img-div"><img className="item-img" src={photo_url.photo_url}/></div>
-            <div className="save-items-button-div"><button className="save-items-button" value={itemId} onClick={handleSavedItems}>Add To Saved</button></div>
-            {/* <div className="bag-button-div"><button className="bag-button" onClick={handleBag}>Add To Bag</button></div> */}
-            <h1 className="word-color">color:</h1>
-            <div className="item-color">{itemValues.colors}</div>
-            <div className="item-sizes">{Object.values(sizesArray).map((size, i) => (
-                <li key={i} className="one-size-list">
-                    <div className="one-size-div">{size.size}</div>
-                </li>
-                ))}
+            <div className="item-img-div">
+                <img className="item-img" src={photo_url.photo_url}/>
             </div>
-            <div className="item-detail">
-                <h3>{itemValues.detail}</h3>
+            <div className="item-info-div">
+                {/* <div className="bag-button-div"><button className="bag-button" onClick={handleBag}>Add To Bag</button></div> */}
+                <h2 className="item-name">{itemValues.itemName}</h2>
+                <h4 className="item-price-sale">OUR PRICE: </h4>
+                <h4 className="item-price-sale">$price</h4>
+                <h4 className="item-price">price</h4>
+                <h3 className="word-color">color: 
+                    <h4>{itemValues.colors}</h4>
+                    <div className="item-color" >
+                        <span class="material-icons" style={{color: itemValues.colors, fontSize: "50px"}}>&#xef4a;</span>
+                    </div>
+                </h3>
+                <div className="sizes">
+                    <h3 className="item-price">Sizes: </h3>
+                    <div className="item-sizes">{Object.values(sizesArray).map((size, i) => (
+                        <li key={i} className="one-size-list" style={{border: "2px solid", width: "8%", height: "10%"}}>
+                            <div className="one-size-div">{size.size}</div>
+                        </li>
+                        ))}
+                    </div>
+                </div>
+                <div className="save-bag-buttons">
+                    <div className="save-button-div"><button className="save-button" value={itemId} onClick={handleSavedItems}>Add To Favorite</button></div>
+                    <div className="bag-button-div"><button className="bag-button" value={itemId} onClick={handleSavedItems}>Add To Bag</button></div>
+                </div>
+                <div className="item-detail">
+                    
+                    <h3>Product Details: </h3>
+                    <h4 style={{width: "50%", fontWeight: "lighter"}}>{itemValues.detail}</h4>
+                </div>
             </div>
+            {showModal && (
+                <div>
+                    <Modal onClose={() => setShowModal(false)}>
+                        <LoginSignUpModal/>
+                    </Modal> 
+                </div>
+            )}
         </div>
     )
 }
