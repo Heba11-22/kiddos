ffd4b4abc486ccb5e5f7850ddfe40d11f987e74f
Heba <hkasaby83@gmail.com>
2021-05-17 09:47:47 -0500

    done

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -5,6 +5,7 @@ function Cart () {
 
     return (
         <div className="cart-div">
+            <h1 className="cart-sen" >The Page is Under Construction</h1>
             <img className="cart-img" src="https://hebacapstone.s3.us-east-2.amazonaws.com/webfactory-ltd-NoOrDKxUfzo-unsplash.jpg"/>
         </div>
     )
